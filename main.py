0ad14979f2735fefc2d657775ace96c54103d925
nyantama <r_nyantama0616@yahoo.co.jp>
2022-06-10 23:16:32 +0900

    successgit add .

---
+++ main.py
@@ -0,0 +1,13 @@
+# import cv2
+# import onnxruntime
+
+
+# # # モデルを読み込む
+# # face_detector = cv2.FaceDetectorYN.create("./model/yunet.onnx", "", (0, 0))
+# print(dir(cv2))
+# # face_recognizer = cv2.FaceRecognizerSF.create("./model/face_recognizer_fast.onnx", "")
+
+import numpy as np
+import cv2
+from insightface.app import FaceAnalysis
+# print(cv2)
